a884cf11fd8017fd3d0e63a071c04fa5d38e031f
Samuel Valencia <104396696+SamuelValencia24@users.noreply.github.com>
2022-07-08 23:50:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿float num,A,B;
+int OPCAO;
+
+Console.WriteLine("Bem vindo a sua Calculadora: ");
+Console.WriteLine("Digite o primeiro valor: ");
+A = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite o Segundo Valor: ");
+B = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Opção 1........Adição");
+Console.WriteLine("Opção 2......Subtração");
+Console.WriteLine("Opção 3.....Multiplicação");
+Console.WriteLine("Opção 4....Divisão");
+Console.WriteLine("Escolha de Uma das Opções: ");
+OPCAO = int.Parse(Console.ReadLine());
+
+    if (OPCAO == 1)
+    {
+        num = A + B;
+        Console.WriteLine("Resultado: " + num);
+    }
+    if (OPCAO == 2)
+    {
+        num = A - B;
+        Console.WriteLine("Resultado: " + num);
+    }
+    if (OPCAO == 3)
+    {
+        num = A * B;
+        Console.WriteLine("Resultado: " + num);
+    }
+    if (OPCAO == 4)
+    {
+        num = A / B;
+        Console.WriteLine("Resultado: " + num);
+    }
+    else if (OPCAO == 0 && OPCAO == 5)
+    {
+        Console.WriteLine("Condição Inválida");
+    }
+
