a30db783b1702c5ad91a19aed375e9f238fdea58
Samuel Valencia <104396696+SamuelValencia24@users.noreply.github.com>
2022-07-08 23:51:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ProjetoPessoa;
+
+Conta conta1 = new Conta();
+
+conta1.saldo = 2.100;
+conta1.limite = 10.000;
+conta1.numero = 221347;
+
+Console.WriteLine("Seu saldo disponivél é de: "+conta1.saldo);
+Console.WriteLine("Seu limite disponivél na conta é de: "+conta1.limite);
+Console.WriteLine("O numero da sua conta é: "+conta1.numero);
+Console.WriteLine("Sua agência agradece!");
+
