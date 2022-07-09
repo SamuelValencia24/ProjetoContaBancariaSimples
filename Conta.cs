00599fd9a6f0a12f5760baa3851294285fe68799
Samuel Valencia <104396696+SamuelValencia24@users.noreply.github.com>
2022-07-08 23:52:20 -0300

    Add files via upload

---
+++ Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPessoa
+{
+    internal class Conta
+    {
+        public double saldo { get; set; }
+        public double limite { get; set; }
+        public int numero { get; set; }
+    }
+}
